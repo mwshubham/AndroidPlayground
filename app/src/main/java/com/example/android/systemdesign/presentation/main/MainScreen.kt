e2e6014cc594b1497e343d66c7b4333705ea8cad
Shubham Agarwal <mwshubham@gmail.com>
2025-11-12 23:03:04 +0530

    Update import statement for Hilt ViewModel in MainScreen.kt

--- app/src/main/java/com/example/android/systemdesign/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/android/systemdesign/presentation/main/MainScreen.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import android.content.res.Configuration
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.android.systemdesign.presentation.components.SystemDesignTopicCard
 import com.example.android.systemdesign.ui.theme.AndroidSystemDesignTheme
 
