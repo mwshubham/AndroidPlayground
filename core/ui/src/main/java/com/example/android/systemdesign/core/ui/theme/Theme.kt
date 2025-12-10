f4b10ca17ce9d8d21c521d87fd307fedf5cd613d
Shubham Agarwal <mwshubham@gmail.com>
2025-12-10 18:39:15 +0530

    refactor: remove unused imports and side effect handling for status bar color in Theme.kt

--- core/ui/src/main/java/com/example/android/systemdesign/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/example/android/systemdesign/core/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.example.android.systemdesign.core.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Primary80,
@@ -102,15 +98,6 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = android.graphics.Color.TRANSPARENT
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
